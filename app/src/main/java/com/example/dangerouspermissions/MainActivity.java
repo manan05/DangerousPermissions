3d763f7d348f831e7b523ced1f5916c0309bd81d
Manan Arora <31929934+manan0313@users.noreply.github.com>
2019-07-30 03:39:06 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/dangerouspermissions/MainActivity.java
+++ app/src/main/java/com/example/dangerouspermissions/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.dangerouspermissions;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -25,16 +29,34 @@ public class MainActivity extends AppCompatActivity {
         btnDial.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String telNo = etPhNo.getText().toString();
-                Uri uri = Uri.parse("tel:" + telNo);
-                Intent i = new Intent(Intent.ACTION_CALL,uri);
-                // We can also write ACTION_VIEW but that will just make the dialer open
-                // This will directly make the call
-                //But it will crash the app and logcat shows an error that we do not have the permission
-                //When clearly we do
-                //This is because CALL is a dangerous permission
-                startActivity(i);
-            }
-        });
+                // Check if we have the permission
+                int perm = ContextCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.CALL_PHONE);
+                if(perm == PackageManager.PERMISSION_GRANTED){
+                    callNumber();
+                }
+                else{
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{
+                                    Manifest.permission.CALL_PHONE},
+                            121);
+                            }};
+                });
+        }
+
+
+    void callNumber(){
+        String telNo = etPhNo.getText().toString();
+        Uri uri = Uri.parse("tel:" + telNo);
+        Intent i = new Intent(Intent.ACTION_CALL,uri);
+
+        // We can also write ACTION_VIEW but that will just make the dialer open
+        // This will directly make the call
+        //But it will crash the app and logcat shows an error that we do not have the permission
+        //When clearly we do
+        //This is because CALL is a dangerous permission
+
+        startActivity(i);
     }
 }
+
