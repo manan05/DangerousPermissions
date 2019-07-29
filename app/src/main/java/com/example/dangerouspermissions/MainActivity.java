4b50b254bec10c0afc95917a95ea8f9d8d162223
Manan Arora <31929934+manan0313@users.noreply.github.com>
2019-07-30 03:24:25 +0530

    Using CALL instead of VIEW

--- app/src/main/java/com/example/dangerouspermissions/MainActivity.java
+++ app/src/main/java/com/example/dangerouspermissions/MainActivity.java
@@ -27,7 +27,12 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String telNo = etPhNo.getText().toString();
                 Uri uri = Uri.parse("tel:" + telNo);
-                Intent i = new Intent(Intent.ACTION_VIEW,uri);
+                Intent i = new Intent(Intent.ACTION_CALL,uri);
+                // We can also write ACTION_VIEW but that will just make the dialer open
+                // This will directly make the call
+                //But it will show an error that we do not have the permission
+                //When clearly we do
+                //This is because CALL is a dangerous permission
                 startActivity(i);
             }
         });
